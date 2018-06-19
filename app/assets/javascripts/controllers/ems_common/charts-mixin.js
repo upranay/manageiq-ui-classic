4396c45a95ca6315ffa051c3417120167cecdcda
Alexander Demichev <ademicev@redhat.com>
2018-06-19 15:26:05 +0200

    add dashboard for ems cloud

--- app/assets/javascripts/controllers/ems_common/charts-mixin.js
+++ app/assets/javascripts/controllers/ems_common/charts-mixin.js
@@ -80,7 +80,7 @@ angular.module('miq.util').factory('chartsMixin', ['$document', function($docume
       chartId: 'recentResourcesChart',
       tooltip: {
         contents: dailyTimeTooltip,
-        position: lineChartTooltipPositionFactory('recentResourcesChart'),
+        position: lineChartTooltipPositionFactory,
       },
       point: {r: 1},
       size: {height: 145},
