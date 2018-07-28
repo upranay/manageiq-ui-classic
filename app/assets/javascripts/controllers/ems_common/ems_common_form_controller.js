dc49afd36757c2fd441e774f73841a210ff2193b
Akshat Mathur <akshat.mathur@metacube.com>
2018-07-28 22:28:01 +0530

    removed system log

--- app/assets/javascripts/controllers/ems_common/ems_common_form_controller.js
+++ app/assets/javascripts/controllers/ems_common/ems_common_form_controller.js
@@ -544,7 +544,6 @@ ManageIQ.angular.app.controller('emsCommonFormController', ['$http', '$scope', '
   };
 
   $scope.telefonicaSecurityProtocolChanged = function() {
-    System.log("hello");
     if ($scope.emsCommonModel.emstype === 'telefonica' || $scope.emsCommonModel.emstype === 'telefonica_infra') {
       if ($scope.emsCommonModel.default_security_protocol === 'non-ssl') {
         $scope.emsCommonModel.default_api_port = $scope.getDefaultApiPort($scope.emsCommonModel.emstype);
