a7eb557f6a7984945c79cb9a24d9335c4977c0fd
Akshat Mathur <akshat.mathur@metacube.com>
2018-09-05 23:09:42 +0530

    conflict resolve

--- app/helpers/vm_helper/textual_summary.rb
+++ app/helpers/vm_helper/textual_summary.rb
@@ -1,4 +1,4 @@
-[-]module VmHelper::TextualSummary
+module VmHelper::TextualSummary
   include TextualMixins::TextualAdvancedSettings
   include TextualMixins::TextualDescription
   include TextualMixins::TextualDrift
@@ -186,8 +186,7 @@
 
   def textual_retirement_date
     # Click2Cloud: Added telefonica cloudmanager template condition
-    return nil if @record.kind_of?(ManageIQ::Providers::Openstack::CloudManager::Template)
-      || @record.kind_of?(ManageIQ::Providers::Telefonica::CloudManager::Template)
+    return nil if @record.kind_of?(ManageIQ::Providers::Openstack::CloudManager::Template) || @record.kind_of?(ManageIQ::Providers::Telefonica::CloudManager::Template)
     {:label => _("Retirement Date"),
      :icon  => "fa fa-clock-o",
      :value => @record.retires_on.nil? ? _("Never") : format_timezone(@record.retires_on)}
